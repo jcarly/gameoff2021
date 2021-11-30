6c614d273a9a37158f53932a8a67cd044334aa42
Jojo <jcarly23@gmail.com>
2021-11-30 22:54:56 +0100

    Save

--- GameOff2021/Assets/Scripts/LayerMovement.cs
+++ GameOff2021/Assets/Scripts/LayerMovement.cs
@@ -12,12 +12,14 @@ public class LayerMovement : MonoBehaviour
     private Vector2 originalPosition;
     private Vector2 originalTargetPosition;
     private Vector2 screenBounds;
+    private Player player;
     // Start is called before the first frame update
     void Start()
     {
         cameraManager = FindObjectOfType<CameraManager>();
+        player = FindObjectOfType<Player>();
         originalPosition = new Vector2(transform.position.x, transform.position.y);
-        originalTargetPosition = new Vector2(cameraManager.transform.position.x, cameraManager.transform.position.y);
+        originalTargetPosition = new Vector2(player.transform.position.x, player.transform.position.y);
         screenBounds = cameraManager.gameObject.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cameraManager.transform.position.z));
         if (looping)
         {
@@ -28,8 +30,8 @@ public class LayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = new Vector3(originalPosition.x + (cameraManager.transform.position.x - originalTargetPosition.x) * horizontalSpeed, originalPosition.y + (cameraManager.transform.position.y - originalTargetPosition.y) * verticalSpeed, transform.position.z);
-        transform.position = Vector3.MoveTowards(
+        Vector3 newPosition = new Vector3(originalPosition.x + (player.transform.position.x - originalTargetPosition.x) * horizontalSpeed, originalPosition.y + (player.transform.position.y - originalTargetPosition.y) * verticalSpeed, transform.position.z);
+        transform.position = Vector3.Lerp(
             transform.position,
             newPosition,
             Time.deltaTime * lerpSpeed
@@ -64,7 +66,7 @@ public class LayerMovement : MonoBehaviour
             Transform firstChild = transform.GetChild(0);
             Transform lastChild = transform.GetChild(count - 1);
             float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x;
-            if (transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)
+            if (player.transform.position.x > lastChild.transform.position.x)
             {
                 firstChild.transform.SetAsLastSibling();
                 firstChild.transform.position = new Vector3(lastChild.position.x + halfObjectWidth * 2, lastChild.position.y, lastChild.transform.position.z);
